cff3fa2fe372d026eb5078fc8129a2b314ba9673
prsonda <prsonda@gmail.com>
2023-09-02 02:44:33 -0300

    Adicionado dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const dotenv = require('dotenv');
+dotenv.config();
+
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 }
 bootstrap();
